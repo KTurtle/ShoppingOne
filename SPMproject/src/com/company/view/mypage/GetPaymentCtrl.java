ff9bc32e4d83e3c1323f08554a986c6cce817591
Kim SoYeon <sopang9@hanmail.net>
2021-05-13 13:55:16 +0900

    shop

---
+++ SPMproject/src/com/company/view/mypage/GetPaymentCtrl.java
@@ -0,0 +1,40 @@
+package com.company.view.mypage;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.company.biz.user.UserVO;
+import com.company.biz.user.impl.UserDAO;
+
+public class GetPaymentCtrl extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		int num = Integer.parseInt(request.getParameter("num"));
+
+		UserDAO dao = new UserDAO();
+		UserVO User = dao.getUser(num);
+
+		request.setAttribute("User", User);
+
+		RequestDispatcher view = request.getRequestDispatcher("joinFormGet.jsp");
+		view.forward(request, response);
+
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		
+		
+	}
+	
+}
