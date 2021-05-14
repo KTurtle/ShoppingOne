899fb2bd612af5f28ca2f8cde0ed7c7490e3ed70
Kim SoYeon <sopang9@hanmail.net>
2021-05-14 13:58:09 +0900

    sd

--- SPMproject/src/com/company/view/product/ViewDetailProductBefore.java
+++ SPMproject/src/com/company/view/product/ViewDetailProductBefore.java
@@ -9,42 +9,34 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.company.biz.product.ProductVO;
+import com.company.biz.product.impl.ProductDAO;
 
-public class ViewDetailProduct extends HttpServlet{
+public class ViewDetailProductBefore extends HttpServlet{
 
-	
-	
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-
+		
 		request.setCharacterEncoding("UTF-8");
 
 		String url = request.getParameter("productURL");
 		
-		// 받을 값을 DAO에 전달하기 위새 1개의 변수로 결합
-		ProductVO productvo = new ProductVO();
-		
-		// url 전달
-		productvo.setProductURL(url);
+		//DAO에 받은 번호를 전달하고 그 결과값을 받는다.
+		ProductDAO dao = new ProductDAO();
+		ProductVO productvo = dao.getProduct(url);
+		//System.out.println(productvo);
 		
 		request.setAttribute("productvo", productvo);
 
 		RequestDispatcher view = request.getRequestDispatcher("detailPageForm.jsp");
 		view.forward(request, response);
-		
-		
+
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-
-		
-		
+		doGet(request, response);
 	}
-	
-	
-	
 	
 }
